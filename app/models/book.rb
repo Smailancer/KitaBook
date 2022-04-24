af121bc46eaeb36a83b977e74d6783e325dcf9a5
smaily1 <b.smaily95@gmail.com>
2022-04-24 15:27:38 +0100

    remove dropbox solution

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,6 @@ class Book < ApplicationRecord
     belongs_to :category
     has_many :reviews
 
-    has_attached_file :book_img, styles: { book_index: "250x350>", book_show: "325x475>" }, default_url: "/images/:style/missing.png",
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    :dropbox_options => {...}
+    has_attached_file :book_img, styles: { book_index: "250x350>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
 end
