19c8ea9e0a7d4908360a0927319a684b1d54e37a
smaily1 <b.smaily95@gmail.com>
2022-05-06 11:40:09 +0100

    Stuck at the params is missing problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , controllers: {
+    registrations: 'registrations',
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :books do 
     resources :reviews
   end
+  resources :categories
   root 'books#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_141057) do
+ActiveRecord::Schema.define(version: 2022_05_04_104937) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_04_10_141057) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -51,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_04_10_141057) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
